c21ebc08312092f4e7dbaac6abb7043d4e5f1ee9
sowon-bak <cruzean479@gmail.com>
2025-11-25 16:26:00 +0900

    transaction models.py 1차 설정

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    # id는 장고가 자동으로 생성
+    # account_id 값을 외래키로 받아옴
+    account_id = models.ForeignKey(
+        "accounts.Accounts",  # 또는 "Account"
+        on_delete=models.CASCADE,
+        related_name="transactions",
+        db_column="account_id",
+    )
+    # 입/출금 구분용 불리언 값. False가 디폴트값. False=출금
+    type = models.BooleanField(default=False)
+    # 입/출금 값
+    amount = models.IntegerField(default=0)
+    # 거래내역 분류
+    category = models.CharField(
+        max_length=8,
+        blank=True,
+    )
+    # 거래내역 상세메모
+    content = models.CharField(
+        max_length=64,
+        blank=True,
+    )
+    # 숨김값 디폴트는 False
+    is_hidden = models.BooleanField(default=False)
+    # 생성시간, 업데이트 시간 단, 입출금값은 업데이트 불가로
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "transaction"
