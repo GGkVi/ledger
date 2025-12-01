a7738d5d64575bf74ea81c118f4d03966c16f336
sowon-bak <cruzean479@gmail.com>
2025-12-01 11:18:12 +0900

    save

---
+++ apps/transactions/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+
+        fields = "__all__"
+
+        read_only_fields = [
+            "id",
+            "account_id",
+            "is_deposit",
+            "amount",
+            "balance_after",
+            "created_at",
+            "updated_at",
+        ]
